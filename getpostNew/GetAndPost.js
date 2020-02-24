791badecd1f920de005631f6e5247c7402ab8054
muse-gabs <45804707+muse-gabs@users.noreply.github.com>
2020-02-24 16:43:26 -0700

    Add files via upload

---
+++ getpostNew/GetAndPost.js
@@ -0,0 +1,80 @@
+var express = require('express');
+
+var app = express();
+var handlebars = require('express-handlebars').create({defaultLayout:'main'});
+
+// this is to parse both URL encoded forms and JSON data 
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+app.set('port', 5437);
+
+app.get('/',function(req,res){
+  res.type('text/plain');
+  res.send('Welcome to the main page!');
+});
+
+app.get('/getAndPost',function(req,res){
+  // create an array to hold the name and values.
+  var daParameterss = [];
+  for (var p in req.query){
+    daParameterss.push({'name':p,'value':req.query[p]})
+  }
+
+  var stuff = {};
+
+  // checks whether there was any data inputted into the array
+  if (daParameterss.length == 0) {
+     stuff.reqMessage = "GET Request was Received but Request was empty"
+  } else {
+     stuff.dataList = daParameterss;
+     stuff.reqMessage = "GET Request Received"
+  }
+     res.render('getAndPost', stuff);
+});
+
+app.post('/getAndPost', function(req,res){
+  var parametersForPost = [];
+  var otherParameters = [];
+
+
+  for (var p in req.query){
+    parametersForPost.push({'name':p,'value':req.query[p]})
+  }
+
+  for (var q in req.body){
+    otherParameters.push({'name':q,'value':req.body[q]})
+  }
+
+  var IDunnoWhatToNameThis = {};
+
+  if (parametersForPost.length == 0 && otherParameters.length == 0) {
+    IDunnoWhatToNameThis.reqMessage = "POST Request was Received but Request was empty"
+  } else {
+    IDunnoWhatToNameThis.dataList = parametersForPost;
+    IDunnoWhatToNameThis.otherDataList = otherParameters;
+    IDunnoWhatToNameThis.reqMessage = "POST Request Received"
+  }
+  res.render('getAndPost', IDunnoWhatToNameThis);
+
+});
+
+app.use(function(req,res){
+  res.status(404);
+  res.render('404');
+});
+
+app.use(function(err, req, res, next){
+  console.error(err.stack);
+  res.type('plain/text');
+  res.status(500);
+  res.render('500');
+});
+
+app.listen(app.get('port'), function(){
+  console.log('Express started on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate.');
+});
